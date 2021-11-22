0542ed3a8b7a92234bc4dac91e87e0bef0a2abe8
cmw1017 <cmw1017@naver.com>
2021-11-22 15:21:58 +0900

    update test_10

--- app.js
+++ app.js
@@ -42,6 +42,9 @@ app.get('/test8', (req, res) => {
 app.get('/test9', (req, res) => {
     res.render('test_09.html');//renger라는게 있음
 });
+app.get('/test10', (req, res) => {
+    res.render('test_10.html');//renger라는게 있음
+});
 app.get('/cylinder', (req, res) => {
     res.render('cylinder.html');//renger라는게 있음
 });
